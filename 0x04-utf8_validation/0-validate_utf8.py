282be02f4800ac1a74286a06424d3f33edd6b9e6
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2023-02-23 20:09:10 +0000

    exe files

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+""" Module to determine whether a given data is valid UTF-8 """
+
+
+def validUTF8(data):
+    """
+    Method that determines given a set of data
+    represents a valid UTF-8 encoding and returns
+    True: if data is a valid UTF-8 enconding
+    False: if not
+    """
+
+    nbytes = 0
+
+    m1 = 1 << 7
+    m2 = 1 << 6
 
     for i in data:
         m = 1 << 7
